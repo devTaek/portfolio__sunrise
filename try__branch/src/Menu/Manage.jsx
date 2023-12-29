751e8dc3102f174c560cbb2683d005da07b1042e
d-taegyun <d.taegyun@gmail.com>
2023-12-29 23:19:40 +0900

    Revert "Revert "[TRY} useContext 적용""
    This reverts commit 733a17eca44d054bbad87dba43e9bac25ae69c60.

---
+++ try__branch/src/Menu/Manage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+
+function Manage() {
+  return (
+    <div>
+      <h1>Manage Page</h1>
+      <Link to='/'>Home</Link>
+
+    </div>
+  )
+}
+
+export default Manage
