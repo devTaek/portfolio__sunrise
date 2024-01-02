fe62c1d846f768105fd722bf47f30d26a189d293
d-taegyun <d.taegyun@gmail.com>
2024-01-02 23:39:21 +0900

    [STYLE] Header : 메뉴 생성

--- src/Page/Header.jsx
+++ src/Page/Header.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './header.scss';
 
 function Header() {
   return (
     <div id='header'>
-      <h1>Header Page</h1>
+      <div className="container">
+        <div className="gap">
+          <Link to="/"><img src='./img/logo_preview_rev_1.png' alt="" /></Link>
+          <div className="headerMenu">
+            <Link to="/players">PLAYERS</Link>
+            <Link to="/match">MATCH</Link>
+            <Link to="/manage">MANAGE</Link>
+            <Link to="/community">COMMUNITY</Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
