535f71ae76f5ede8d1b575bc9391e92a056c5382
d-taegyun <d.taegyun@gmail.com>
2024-01-03 23:26:39 +0900

    [FIX] ENOENT 오류 및 Main 페이지 수정

--- src/Page/Main/Main.jsx
+++ src/Page/Main/Main.jsx
@@ -1,33 +1,33 @@
-import React,{useState,useEffect} from 'react'
+import React from 'react'
 import './scss/Main.scss'
 
 function Main() {
-  const [position, setPosition] = useState(0);
+  // const [position, setPosition] = useState(0);
 
-  function onScroll() {
-    setPosition(window.scrollY);
-  }
-  useEffect(()=>{
-    window.addEventListener("scroll", onScroll);
-    return ()=> {
-      window.removeEventListener("scroll", onScroll)
-    };
-  },[])
+  // function onScroll() {
+  //   setPosition(window.scrollY);
+  // }
+  // useEffect(()=>{
+  //   window.addEventListener("scroll", onScroll);
+  //   return ()=> {
+  //     window.removeEventListener("scroll", onScroll)
+  //   };
+  // },[])
   return (
     <main id='main'>
       <section className="container">
         <div className="gap">
-          <article className="mainPlayers" style={{transform: `translateX(${position}px)`}}>
+          <article className="mainPlayers" >
             <img src='./img/4254b1223bdb-dybala21-copia.png' alt="" />
             <h3>정태균</h3>
           </article>
-          <article className="mainMatch" style={{transform: `translateX(${-position}px)`}}>
+          <article className="mainMatch">
             <h3>MATCH</h3>
           </article>
-          <article className="mainManage" style={{transform: `translateX(${position}px)`}} >
+          <article className="mainManage">
             <h3>MANAGE</h3>
           </article>
-          <article className="mainCommunity" style={{transform: `translateX(${-position}px)`}}>
+          <article className="mainCommunity">
             <h3>COMMUNITY</h3>
           </article>
         </div>
