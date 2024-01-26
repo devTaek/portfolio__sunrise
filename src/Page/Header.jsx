fa58b8fedc104e6d6419dfbaa57927620bc93c5d
d-taegyun <d.taegyun@gmail.com>
2024-01-26 19:40:04 +0900

    [TRY] 모바일 헤더바 시도

--- src/Page/Header.jsx
+++ src/Page/Header.jsx
@@ -1,25 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
+import SideBar from './SideBar';
 
 import './scss/header.scss';
 
 
 
 function Header() {
+
+  const [isOpen, setIsOpen] = useState(false);
+  
+  const toggleSide = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <header id='header'>
       <div className="container">
         <div className="gap">
           <Link to="/"><img src='./img/pc-logo.png' alt="" /></Link>
-          <div className="headerMenu">
+          <div className="header__menu">
             <Link to="/players">PLAYERS</Link>
             <Link to="/match">MATCH</Link>
             <Link to="/manage">MANAGE</Link>
             <Link to="/community">COMMUNITY</Link>
           </div>
-          <div className='mobile-bar'>'
-            <a class="glyphicon glyphicon-menu-hamburger"></a>
-          </div>
+          <button onClick={toggleSide} className="menuBtn"><i className='material-icons'>&#xe5d2;</i></button>
+          {isOpen ? (
+            <ul>
+              <li>PLAYERS</li>
+              <li>MATCH</li>
+              <li>MANAGE</li>
+              <li>COMMUNITY</li>
+            </ul>
+          ):''}
         </div>
       </div>
     </header>
