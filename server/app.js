fa703af8d4f4886a5ca15297311213a2a0cfe7ab
d-taegyun <d.taegyun@gmail.com>
2024-04-26 19:12:06 +0900

    [UPDATE] app.use의 build경로 추가

--- server/app.js
+++ server/app.js
@@ -1,11 +1,17 @@
 
 const express = require('express');
+const path = require('path');
 const app = express();
 const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());   // for parsing application/json
 app.use(express.urlencoded({ extended: true }));   // for parsing application/x-www-form-urlencoded
+
+app.use(express.static(path.join(__dirname, '../build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
 
 const { playersList } = require('./data/playersList.json');
 const { matchList } = require('./data/matchList.json');
