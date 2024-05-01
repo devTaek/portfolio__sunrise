7b23e3868ec5d7bdc5e0b09f870a8395dcd5cf12
d-taegyun <d.taegyun@gmail.com>
2024-05-01 21:25:50 +0900

    [FIX] 서버 포트 재수정.(3000 > 3001)

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,6 +1,6 @@
 const app = require("../app");
-const PORT = 3000;
+const PORT = 3001;
 
 app.listen(PORT, () => {
-  console.log("server works on port : " + PORT);
+  console.log("server works on port : " + PORT)
 })
