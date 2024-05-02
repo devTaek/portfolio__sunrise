a4db8709c7447cb1f12a985da083f1c7d6ae6a2f
d-taegyun <d.taegyun@gmail.com>
2024-05-03 02:49:47 +0900

    [ADD] CORS 에러. method, mode 추가.

--- client/src/container/common/utils/fetchData.js
+++ client/src/container/common/utils/fetchData.js
@@ -1,6 +1,8 @@
 export async function fetchData(page, setData) {
   try {
     const response = await fetch(`http://15.164.204.237/:5000/api/${page}`, {
+      method: 'get',
+      mode: 'no-cors',
       headers: {
         'Content-Type': 'application/json', 
         'Access-Control-Allow-Origin': 'http://15.164.204.237:5000'
