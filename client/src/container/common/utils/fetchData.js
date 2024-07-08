1ba6b1243afcf7f9dda4025a68b9c47ddffa8873
d-taegyun <d.taegyun@gmail.com>
2024-07-08 21:08:51 +0900

    [REFACTOR] cors 세부 설정

--- client/src/container/common/utils/fetchData.js
+++ client/src/container/common/utils/fetchData.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 export async function fetchData(page, setData) {
   try {
-    const response = await axios.get(`http://localhost:3001/api/${page}`);
+    const response = await axios.get(`http://localhost:3001/api/${page}`, {
+      withCredentials: true
+    });
     setData(response.data);
   } catch (error) {
     console.error('Error fetching data: ', error);
--- server/app.js
+++ server/app.js
@@ -4,7 +4,10 @@ const app = express();
 const cors = require('cors');
 const fs = require('fs');
 
-app.use(cors());
+app.use(cors({
+  origin: `http://localhost:3000`,
+  credentials: true,
+}));
 app.use(express.json());   // for parsing application/json
 app.use(express.urlencoded({ extended: true }));   // for parsing application/x-www-form-urlencoded
 
